12ef3da386433a2bb27a131e8230bcaa622a60d4
enxajt <enxajt@gmail.com>
2018-03-16 15:46:38 +0900

    sth

--- desktop/start-up.sh
+++ desktop/start-up.sh
@@ -3,25 +3,17 @@
 # ~/.xbindkeysrc volume, backlight
 xbindkeys
 
-# Device Accel Velocity Scaling : speed
+syndaemon -i 1 -d -t -K # Disable touchpad while typing
+# speed (default=12.5)
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Accel Velocity Scaling' 14
 # Acceleration: small is fast
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Accel Constant Deceleration' 1.0
+#xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Accel Constant Deceleration' 1.0
 # Natural scrolling
 xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Scrolling Distance' -119 -119
 # Horizontal scrolling
 xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Two-Finger Scrolling' 1 1
 
-# Disable touchpad while typing
-syndaemon -i 1 -d -t -K
-
-# https://ubuntuforums.org/showthread.php?t=1966900
-#xrandr --output eDP1 --mode "1600x900"
-#xrandr --output DP1 --pos 1600x0 --above eDP1
-xrandr --output eDP1 --mode 1600x900 --output DP1 --auto --above eDP1 # VGA@KRC
-xrandr --output eDP2 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --rotate normal --above eDP1
-
 redshift -O 3200
-#firefox&
 dropbox lansync n
 dropbox start
 #sh ~/.dotfiles/always.sh &
@@ -29,9 +21,29 @@ dropbox start
 
 mvt $home/.local/share/recently-used.xbel
 
-sleep 2s
+#pavucontrol
+#pulseaudio
+#amixer
+mvt ~/.config/pulse
+pulseaudio -k
+pulseaudio --start
+
+sudo apt update 
+echo "\n" 
+sudo apt upgrade -y 
+echo "\n" 
+sudo apt autoremove -y
+echo "\n" 
+sudo apt dist-upgrade
+
+sleep 1s
 setxkbmap -option "ctrl:swapcaps"
 setxkbmap -option "swapcaps:ctrl"
+
+xrandr --output eDP1 --mode 1600x900 --output DP1 --auto --above eDP1 #VGA@KRC
+xrandr --output eDP1 --mode 1600x900 --output HDMI1 --auto --above eDP1 #DELL@Kuya
+#xrandr --output eDP1 --mode 1600x900 --output HDMI1 --mode 1920x1080 --rotate normal --above eDP1
+#xrandr --output eDP1 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --rotate normal --above eDP1
 
 # for mate not i3
 #wmctrl -r "termite" -b toggle,maximized_vert,maximized_horz
