fe8341ae5ffa7498c5be85b4d20009ea20448205
enxajt <enxajt@gmail.com>
2019-08-30 18:12:04 +0900

    sth

--- desktop/start-up.sh
+++ desktop/start-up.sh
@@ -8,22 +8,24 @@ xinput set-prop 'DELL07E6:00 06CB:76AF Touchpad' 'libinput Accel Speed' 1
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target # disable suspend
 #sudo systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target # re-enable suspend
 
-rm $home/.local/share/recently-used.xbel
+rm $HOME/.local/share/recently-used.xbel
 
 echo "\n# dropbox"
 dropbox lansync n
 dropbox start
 
-echo "\n# redshift, desktop background, kbd_backlight"
-redshift -O 4200
-xsetroot -solid "#000000" # Desktop background
-echo 0 | sudo tee /sys/class/leds/dell::kbd_backlight/brightness
-
 echo "\n# xrandr"
-#xrandr --output DP2 --mode 1600x1200 --above eDP1 # KRC Projector
 xrandr --output eDP1 --mode 1920x1080
 xrandr --output DP1 --auto --above   eDP1 --rotate normal
-xrandr --output DP2 --auto --right-of DP1 --rotate normal # 起動時はDPのみ、後にVGA追加
+xrandr --output DP2 --auto --right-of DP1 --rotate normal
+feh --bg-center $HOME/enx/log/enx-分類/DL/Art/Manabu_Ikeda_Rebirth.jpg
+#xrandr --output DP2 --mode 1600x1200 --above eDP1 # KRC Projector
+#feh --bg-center $HOME/enx/log/enx-分類/DL/Art/Animals1419.png
+#xsetroot -solid "#000000" # Desktop background
+
+echo "\n# redshift, desktop background, kbd_backlight"
+redshift -O 4200
+echo 0 | sudo tee /sys/class/leds/dell::kbd_backlight/brightness
 
 echo "\n# backlight"
 STATE=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state`
